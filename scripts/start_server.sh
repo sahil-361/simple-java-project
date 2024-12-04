26453f8244ebb99da4b0d24a17ce42149aa48e76
Sahil Garg <Sahil.Garg@evryindia.in>
2024-12-04 09:07:41 +0530

    script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-sudo yum update
+sudo yum update -y
+sudo yum install wget -y
+
 sudo amazon-linux-extras enable corretto8
 sudo yum install java-1.8.0-amazon-corretto -y
 sudo yum install maven
-sudo yum install gradle -y
+
+# Install Gradle manually
+echo "Installing Gradle..."
+GRADLE_VERSION=7.6
+wget https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip -P /tmp
+sudo unzip -d /opt/gradle /tmp/gradle-${GRADLE_VERSION}-bin.zip
+sudo ln -s /opt/gradle/gradle-${GRADLE_VERSION}/bin/gradle /usr/bin/gradle
+
+# Verify installations
+echo "Java version:"
+java -version
+echo "Gradle version:"
+gradle -v
 
 echo "Starting Java application..."
 sudo systemctl start simple-java-project
-sudo systmectl enable simple-java-project
+sudo systemctl enable simple-java-project
